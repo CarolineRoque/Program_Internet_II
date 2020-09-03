5c0351d2b0e36905650c84f2ab03dbad4a453bcc
CarolineRoque <carolineramosroque96@gmail.com>
2020-09-03 11:51:05 -0300

    aula03

---
+++ aula03/servidor.php
@@ -0,0 +1,16 @@
+<?php
+
+$numero = $_GET['txtValor'];
+$conteudo = "<table border='1'>
+                <tr>
+                    <th>Valores</th>
+                    </tr>";
+for ($i = $numero + 1 ; $i<= $numero + 10; $i ++){
+    $conteudo = $conteudo . "<tr>
+                                <td>".$i."</td>
+                                </tr>       ";
+}
+
+$conteudo = $conteudo . "</table>";
+
+echo $conteudo;
